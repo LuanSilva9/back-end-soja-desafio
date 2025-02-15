1f15af03d4341b7b75b26f3bbb0d18f5c39a4be7
LuanSilva9 <luanpdev@gmail.com>
2025-02-15 18:08:39 -0300

    featr: Criando modulo de mudança de status, metodo PUT já acessivel

--- src/main/java/com/desafio/tecnico/soja/service/TransporteService.java
+++ src/main/java/com/desafio/tecnico/soja/service/TransporteService.java
@@ -4,6 +4,7 @@ import com.desafio.tecnico.soja.domain.model.Lote.Lote;
 import com.desafio.tecnico.soja.domain.model.Transporte.Transporte;
 import com.desafio.tecnico.soja.domain.repository.Transporte.TransporteRepository;
 import com.desafio.tecnico.soja.dto.TransportDTO;
+import com.desafio.tecnico.soja.dto.UpdateTransportStatusDTO;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,5 +30,14 @@ public class TransporteService {
         Transporte newTransporte = new Transporte(data, lote);
 
         return this.transporteRepository.save(newTransporte);
+    }
+
+    @Transactional
+    public Transporte updateTransportStatus(Long id, UpdateTransportStatusDTO data) throws Exception {
+        Transporte transporte = this.getTransporteById(id);
+
+        transporte.setStatus(data.status());
+
+        return transporteRepository.save(transporte);
     }
 }
