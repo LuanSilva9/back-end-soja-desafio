b3c81b28f86cae3697e05f0f10394c114e68f60d
LuanSilva9 <luanpdev@gmail.com>
2025-02-15 16:47:08 -0300

    featr: Adicionando status / Similar ao status do transporte

--- src/main/java/com/desafio/tecnico/soja/dto/ExportacaoDTO.java
+++ src/main/java/com/desafio/tecnico/soja/dto/ExportacaoDTO.java
@@ -1,6 +1,8 @@
 package com.desafio.tecnico.soja.dto;
 
+import com.desafio.tecnico.soja.domain.model.Transporte.TransportStatus;
+
 import java.time.LocalDate;
 
-public record ExportacaoDTO(Long lote, String paisDestino, String paisEmbarque, LocalDate dataExportacao) {
+public record ExportacaoDTO(Long loteId, String paisDestino, String paisEmbarque, LocalDate dataExportacao, TransportStatus status) {
 }
