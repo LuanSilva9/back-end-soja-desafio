87594b9dc67206807f87add411d5db6248e67968
LuanSilva9 <luanpdev@gmail.com>
2025-02-15 12:20:26 -0300

    featr: Alterando para usar variavel de ambiente

--- src/main/java/com/desafio/tecnico/soja/SojaApplication.java
+++ src/main/java/com/desafio/tecnico/soja/SojaApplication.java
@@ -1,5 +1,7 @@
 package com.desafio.tecnico.soja;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SojaApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(SojaApplication.class, args);
 	}
 
