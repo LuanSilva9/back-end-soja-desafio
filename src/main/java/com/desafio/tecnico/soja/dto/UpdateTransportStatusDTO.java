cde31870ce0bd31449adf904e896076e7df69589
LuanSilva9 <luanpdev@gmail.com>
2025-02-15 18:06:50 -0300

    featr: Criando dto personalizado para switch de status

---
+++ src/main/java/com/desafio/tecnico/soja/dto/UpdateTransportStatusDTO.java
@@ -0,0 +1,6 @@
+package com.desafio.tecnico.soja.dto;
+
+import com.desafio.tecnico.soja.domain.model.Transporte.TransportStatus;
+
+public record UpdateTransportStatusDTO(TransportStatus status) {
+}
