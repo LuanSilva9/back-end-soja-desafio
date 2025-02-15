fbf9e38a039fdf5bfa1b4065ee31ce8266ce0875
LuanSilva9 <luanpdev@gmail.com>
2025-02-15 11:56:47 -0300

    Criando arquivos de configuração para inicio do projeto spring

---
+++ src/main/java/com/desafio/tecnico/soja/SojaApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.tecnico.soja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SojaApplication.class, args);
+	}
+
+}
