7cc3c982a7536309444e08131e384635e4c28f44
LuanSilva9 <luanpdev@gmail.com>
2025-02-15 18:07:29 -0300

    featr: Criando modulo de busca de lote por fazenda

--- src/main/java/com/desafio/tecnico/soja/domain/repository/Lote/LoteRepository.java
+++ src/main/java/com/desafio/tecnico/soja/domain/repository/Lote/LoteRepository.java
@@ -1,12 +1,15 @@
 package com.desafio.tecnico.soja.domain.repository.Lote;
 
+import com.desafio.tecnico.soja.domain.model.Fazenda.Fazenda;
 import com.desafio.tecnico.soja.domain.model.Lote.Lote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LoteRepository extends JpaRepository<Lote, Long> {
     Optional<Lote> findLoteById(Long id);
+    Optional<List<Lote>> findLoteByFazenda(Fazenda fazenda);
 }
